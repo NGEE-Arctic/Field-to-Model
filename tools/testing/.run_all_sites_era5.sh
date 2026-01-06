90cd9f13541f5249a30a11a8eea11feb9c59237c
Rich Fiorella <rfiorella@lanl.gov>
2026-01-06 16:33:45 -0700

    Add E3SM to model container, fix vis_notebook path

--- tools/testing/.run_all_sites_era5.sh
+++ tools/testing/.run_all_sites_era5.sh
@@ -10,6 +10,8 @@ sites=(
     abisko
     samoylov_island
     bayelva
+    imnaviat_creek
+    upper_kuparuk
 )
 
 # a couple assumptions here that are different
@@ -18,29 +20,40 @@ sites=(
 #    and not docker volumes, so I'm not using volumes.
 # 2) I am redirecting output to log files so I can run all sites at once
 
+job_count=0
+
 for site in "${sites[@]}"; do
-    docker run --rm -v /code/field-to-model-inputdata:/mnt/inputdata \
-        -v /data/ngee-baseline-runs:/mnt/output \
-        -v $(pwd):/home/modex_user \
+
+    cpu=${job_count}
+    echo "Running control for ${site} on CPU ${job_count}"
+    docker run --pull always --cpuset-cpus "${cpu}" -v inputdata:/mnt/inputdata \
+        -v output:/mnt/output \
+        -v $(pwd)/model_examples:/home/modex_user/model_examples \
+        -v $(pwd)/E3SM:/home/modex_user/E3SM \
         -u modex_user \
         -e USER=modex_user \
         yuanfornl/ngee-arctic-modex26:models-main-latest \
         /home/modex_user/model_examples/ELM/run_ngeearctic_site.sh \
-        --site_name=${site} --case_prefix="cntl" > logs/${site}-cntl.log 2>&1 &
+        --site_name=${site} --case_prefix="cntl" > logs/${site}-era5-cntl.log 2>&1 &
 
-    sleep 120
+    ((job_count++))
+    sleep 600
 
-    docker run --rm -v /code/field-to-model-inputdata:/mnt/inputdata \
-        -v /data/ngee-baseline-runs:/mnt/output \
-        -v $(pwd):/home/modex_user \
+    cpu=${job_count}
+    echo "Running arctic initialization for ${site} on CPU ${job_count}"
+    docker run --cpuset-cpus "${cpu}" --rm -v inputdata:/mnt/inputdata \
+        -v output:/mnt/output \
+        -v $(pwd)/model_examples:/home/modex_user/model_examples \
+        -v $(pwd)/E3SM:/home/modex_user/E3SM \
         -u modex_user \
         -e USER=modex_user \
         yuanfornl/ngee-arctic-modex26:models-main-latest \
         /home/modex_user/model_examples/ELM/run_ngeearctic_site.sh \
         --site_name=${site} --case_prefix="ArcInit" --use_arctic_init \
-        > logs/${site}-arcinit.log 2>&1 &
+        > logs/${site}-era5-arcinit.log 2>&1 &
 
-   sleep 120
+    sleep 600
+    ((job_count++))
 
 done
 
