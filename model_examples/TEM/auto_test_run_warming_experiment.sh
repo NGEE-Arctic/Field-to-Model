7d4bc9b2d4e2453c6baf10541f38749efa020faa
Tobey Carman <tcarman2@alaska.edu>
2026-01-08 21:36:07 -0900

    Generalize auto test runner to be able to handle...
    ... automatic discovery of the length of the transient data.

--- model_examples/TEM/auto_test_run_warming_experiment.sh
+++ model_examples/TEM/auto_test_run_warming_experiment.sh
@@ -12,25 +12,31 @@
 
 INPUT_DATA_NAME=$1
 
-# Setup and run the 
+# Setup for both runs
 mkdir -p /mnt/output/tem/tem_ee1_warming
 cd /mnt/output/tem/tem_ee1_warming
+
+# Setup the control run
 pyddt-swd --input-data-path /mnt/inputdata/TEM/$INPUT_DATA_NAME --copy-inputs --force control
 cd control/
+# Figure out how many years to run (how long is the historic data?)
+TRANSIENT_YRS=$(python -c "import xarray; ds = xarray.open_dataset('inputs/$INPUT_DATA_NAME/historic-climate.nc'); print(int(ds.sizes['time']/12))")
 pyddt-outspec config/output_spec.csv --on GPP m p
 pyddt-outspec config/output_spec.csv --on LAYERDZ m l
 pyddt-outspec config/output_spec.csv --on TLAYER m l
 pyddt-outspec config/output_spec.csv --on CMTNUM y
 pyddt-outspec config/output_spec.csv -s
-dvmdostem -f config/config.js -p 10 -e 10 -s 25 -t 123 -n 0 --force-cmt 6 -l monitor
+dvmdostem -f config/config.js -p 10 -e 10 -s 25 -t $TRANSIENT_YRS -n 0 --force-cmt 6 -l monitor
 
+# Setup the warming run
 cd /mnt/output/tem/tem_ee1_warming
 pyddt-swd --input-data-path /mnt/inputdata/TEM/$INPUT_DATA_NAME --copy-inputs --force warming_2.6C_JJAS_2019
-cd /mnt/output/tem/tem_ee1_warming/warming_2.6C_JJAS_2019
+cd warming_2.6C_JJAS_2019
 python /home/modex_user/model_examples/TEM/modify_air_temperature.py --input-file inputs/$INPUT_DATA_NAME/historic-climate.nc --months 6 7 8 9 --years 2019 --deviation 2.6
 mv inputs/$INPUT_DATA_NAME/modified_historic-climate.nc inputs/$INPUT_DATA_NAME/historic-climate.nc
 cp ../control/config/output_spec.csv config/output_spec.csv
-dvmdostem -f config/config.js -p 10 -e 10 -s 25 -t 123 -n 0 -l monitor --force-cmt 6
+# length of historic data should be the same as control run
+dvmdostem -f config/config.js -p 10 -e 10 -s 25 -t $TRANSIENT_YRS -n 0 -l monitor --force-cmt 6
 
 
 
