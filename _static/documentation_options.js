1cb303790e1404f211f2499f2e89a1f3b0418273
rfiorella <rfiorella@users.noreply.github.com>
2026-01-14 20:36:02 +0000

    deploy: a7d35a9b134cb88ab5a111980000770194fa39b1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '(updated: Jan 14 20:29:32 UTC)',
+    VERSION: '(updated: Jan 14 20:35:57 UTC)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
